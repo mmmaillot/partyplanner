e657c9b883364c405c398949a49ee8a8b28fcbbf
mmmaillot <maxime.m.maillot@gmail.com>
2016-02-06 14:41:35 +0100

    Hello World

---
+++ src/main/java/org/cocktailmolotov/partyplanner/Main.java
@@ -0,0 +1,15 @@
+package org.cocktailmolotov.partyplanner;
+
+import java.io.IOException;
+
+/**
+ * Created by maxime on 06/02/16.
+ */
+public class Main {
+
+    public static void main(String args[]) throws IOException {
+        System.out.println("Hello World");
+        System.in.read();
+        System.out.println("I read !");
+    }
+}
