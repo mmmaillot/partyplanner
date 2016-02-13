58b837a8f5a43e8a62e71639360d8c4f71b88a7c
mmmaillot <maxime.m.maillot@gmail.com>
2016-02-14 00:53:45 +0100

    tostring for cocktails

--- src/main/java/org/cocktailmolotov/partyplanner/dto/Cocktails.java
+++ src/main/java/org/cocktailmolotov/partyplanner/dto/Cocktails.java
@@ -3,13 +3,13 @@ package org.cocktailmolotov.partyplanner.dto;
 import java.util.List;
 
 public class Cocktails {
-	
+
 	private List<Cocktail> cocktails;
 
 	public Cocktails() {
 		// no attributes needed
 	}
-	
+
 	public List<Cocktail> getCocktails() {
 		return cocktails;
 	}
@@ -17,6 +17,14 @@ public class Cocktails {
 	public void setCocktails(List<Cocktail> cocktails) {
 		this.cocktails = cocktails;
 	}
-	
+
+	@Override
+	public String toString() {
+		String result = new String();
+		for(Cocktail cocktail : cocktails){
+			result += cocktail.toString() + "\n";
+		}
+		return result;
+	}
 
 }
