d607e15302d139e395677fbc89aa6b9f4b9fdaa2
mmmaillot <maxime.m.maillot@gmail.com>
2016-02-16 00:12:16 +0100

    base cocktails and base ingredients created, time for serious business

--- src/main/java/org/cocktailmolotov/partyplanner/Main.java
+++ src/main/java/org/cocktailmolotov/partyplanner/Main.java
@@ -2,17 +2,16 @@ package org.cocktailmolotov.partyplanner;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import org.cocktailmolotov.partyplanner.dto.Cocktail;
 import org.cocktailmolotov.partyplanner.dto.Cocktails;
+import org.cocktailmolotov.partyplanner.dto.Ingredients;
 import org.cocktailmolotov.partyplanner.io.CocktailLoader;
 import org.json.simple.parser.ParseException;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
+import java.util.Map.Entry;
 
 
 /**
@@ -20,43 +19,21 @@ import java.util.List;
  */
 public class Main {
 
-    public static void main(String args[]) throws IOException, URISyntaxException, ParseException {
-    	
-    	File file = CocktailLoader.getLoader().getCocktailsFile();
+	public static void main(String args[]) throws IOException, URISyntaxException, ParseException {
 
-    	ObjectMapper om = new ObjectMapper();
-        Cocktail mizuwari = createMizuwariCocktail();
-        Cocktail daiquiri =  createDaiquiriCocktail();
-        HashMap<String,HashMap<String,Integer>> cocktailMap = new HashMap<>();
-        cocktailMap.put(mizuwari.getId(),mizuwari.getDoses());
-        cocktailMap.put(daiquiri.getId(),daiquiri.getDoses());
-        Cocktails cocktails = new Cocktails();
-        cocktails.setCocktails(cocktailMap);
-//        String cocktailFile = "/home/maxime/Documents/cocktails.json";
-        om.writerWithDefaultPrettyPrinter().writeValue(file, cocktails);
-        System.out.println("done");
-        Cocktails cocktail = om.readValue(file, Cocktails.class);
-        System.out.println(cocktail);
-    }
-
-    private static Cocktail createMizuwariCocktail(){
-        Cocktail cocktail = new Cocktail();
-        cocktail.setId("Mizuwari");
-        HashMap<String,Integer> doses = new HashMap<>();
-        doses.put("water", 50);
-        doses.put("whisky",50);
-        cocktail.setDoses(doses);
-        return cocktail;
-    }
-    
-    private static Cocktail createDaiquiriCocktail(){
-        Cocktail cocktail = new Cocktail();
-        cocktail.setId("Daiquiri");
-        HashMap<String,Integer> doses = new HashMap<>();
-        doses.put("rhum", 60);
-        doses.put("lime juice",30);
-        doses.put("simple syrup",15);
-        cocktail.setDoses(doses);
-        return cocktail;
-    }
+		File file = CocktailLoader.getLoader().getCocktailsFile();
+		ObjectMapper om = new ObjectMapper();
+		Cocktails cocktails = om.readValue(file, Cocktails.class);
+		HashMap<String, Double> ingredientsMap = new HashMap<>();
+		for(HashMap<String, Double> cocktail : cocktails.getCocktails().values()){
+			for(String ingred : cocktail.keySet()){
+				ingredientsMap.put(ingred, 10.);
+				System.out.println(ingred);
+			}
+		}
+		Ingredients ingredients = new Ingredients();
+		ingredients.setIngredients(ingredientsMap);
+		file = CocktailLoader.getLoader().getIngredientsFile();
+		om.writerWithDefaultPrettyPrinter().writeValue(file, ingredients);
+	}
 }
--- src/main/java/org/cocktailmolotov/partyplanner/dto/Cocktails.java
+++ src/main/java/org/cocktailmolotov/partyplanner/dto/Cocktails.java
@@ -6,24 +6,24 @@ import java.util.Map.Entry;
 
 public class Cocktails {
 
-	private HashMap<String,HashMap<String,Integer>> cocktails;
+	private HashMap<String,HashMap<String,Double>> cocktails;
 
 	public Cocktails() {
 		// no attributes needed
 	}
 
-	public HashMap<String,HashMap<String,Integer>> getCocktails() {
+	public HashMap<String,HashMap<String,Double>> getCocktails() {
 		return cocktails;
 	}
 
-	public void setCocktails(HashMap<String,HashMap<String,Integer>> cocktails) {
+	public void setCocktails(HashMap<String,HashMap<String,Double>> cocktails) {
 		this.cocktails = cocktails;
 	}
 
 	@Override
 	public String toString() {
 		String result = new String();
-		for(Entry<String,HashMap<String,Integer>> cocktail : cocktails.entrySet()){
+		for(Entry<String,HashMap<String,Double>> cocktail : cocktails.entrySet()){
 			result += cocktail.toString() + "\n";
 		}
 		return result;
