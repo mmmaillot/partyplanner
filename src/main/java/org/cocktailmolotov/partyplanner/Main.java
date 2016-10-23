bd54bc80ce0ddfc291f3f89359f54def317d8071
mmmaillot <maxime.m.maillot@gmail.com>
2016-10-23 17:32:23 +0200

    apche cli added

--- src/main/java/org/cocktailmolotov/partyplanner/Main.java
+++ src/main/java/org/cocktailmolotov/partyplanner/Main.java
@@ -2,6 +2,10 @@ package org.cocktailmolotov.partyplanner;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
 import org.cocktailmolotov.partyplanner.dto.Cocktail;
 import org.cocktailmolotov.partyplanner.dto.Cocktails;
 import org.cocktailmolotov.partyplanner.dto.Ingredients;
@@ -20,9 +24,20 @@ import java.util.Map.Entry;
  */
 public class Main {
 
-	public static void main(String args[]) throws IOException, URISyntaxException, ParseException {
+	public static void main(String args[]) throws IOException, URISyntaxException, ParseException, org.apache.commons.cli.ParseException {
+
+		// create Options object
+		Options options = new Options();
+		options.addOption("n", true, "The number of person attending the party");
+
+		CommandLineParser parser = new DefaultParser();
+		CommandLine cmd = parser.parse( options, args);
+
 
 		int quantite = 10;
+		if(cmd.hasOption("n")){
+			quantite = Integer.parseInt(cmd.getOptionValue("n"));
+		}
 		HashMap<String, Integer> groceries = new HashMap<>();
 
 		Cocktails cocktails = CocktailLoader.getLoader().loadCocktails();
